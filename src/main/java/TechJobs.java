bde20118e7c3ab3d67674248adcb3f87b01ec56a
Roger Thomas <rogerthomas96@outlook.com>
2024-04-20 15:12:23 -0500

    passed first test

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,25 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println();
+            System.out.println("*****");
+            System.out.println("position type: " + job.get("position type"));
+            System.out.println("name: " + job.get("name"));
+            System.out.println("employer: " + job.get("employer"));
+            System.out.println("location: " + job.get("location"));
+            System.out.println("core competency: " + job.get("core competency"));
+            System.out.println("*****");
+        }
+        //match found format
+//        System.out.println("printJobs is not implemented yet");
+//
+//
+//        //no results format
+//        System.out.println("Search term:");
+//        System.out.println("Example Search Term with No Results");
+//        System.out.println("No Results");
+        
     }
 }
